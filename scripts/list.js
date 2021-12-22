870c4ddf18c740566caa811227b6757f571c61b9
SHIVSB <2020kuec2005@iiitkota.ac.in>
2021-12-23 02:02:41 +0530

    Checkpoint 8

---
+++ scripts/list.js
@@ -0,0 +1,42 @@
+
+const first = document.getElementById('toggle1');
+const second = document.getElementById('toggle2');
+
+first.addEventListener('click', () => {
+  first.style.backgroundColor = 'dodgerblue';
+  second.style.backgroundColor = 'white';
+});
+
+second.addEventListener('click', () => {
+  second.style.backgroundColor = 'dodgerblue';
+  first.style.backgroundColor = 'white';
+});
+
+
+const targetDiv1 = document.getElementById("third");
+const targetDiv2 = document.getElementById("fourth");
+
+const btn1 = document.getElementById("toggle1");
+btn1.onclick = function() {
+    if (targetDiv1.style.display !== "none") {
+        targetDiv1.style.display = "none";
+        targetDiv2.style.display = "block";
+    } else {
+        targetDiv2.style.display = "block";
+    }
+
+};
+
+const btn2 = document.getElementById("toggle2");
+btn2.onclick = function() {
+    if (targetDiv2.style.display !== "none") {
+        targetDiv2.style.display = "none";
+        targetDiv1.style.display = "block";
+    } else {
+        targetDiv1.style.display = "block";
+    }
+};
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    document.getElementById("toggle1").click();
+});
